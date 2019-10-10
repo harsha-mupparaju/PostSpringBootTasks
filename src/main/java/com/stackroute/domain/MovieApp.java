322fe1e528a9d5d316fd9008951bd962d53f0efc
Your Name <youremail@domain.com>
2019-10-10 11:04:30 +0530

    ninth task

--- src/main/java/com/stackroute/domain/MovieApp.java
+++ src/main/java/com/stackroute/domain/MovieApp.java
@@ -3,80 +3,86 @@ package com.stackroute.domain;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity(name = "movie")
 @ApiModel
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class MovieApp {
 
     @Id
     @ApiModelProperty
-      String id;
+    String id;
     @ApiModelProperty
     String title;
     @ApiModelProperty
-      String genre;
+    String genre;
     @ApiModelProperty
-     String language;
-
-    public MovieApp() {
-    }
-
-    public MovieApp(String id, String title, String genre, String language) {
-        this.id = id;
-        this.title = title;
-        this.genre = genre;
-        this.language = language;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public String getLanguage() {
-        return language;
-    }
-
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
-
-    @Override
-    public String toString() {
-        return "MovieApp{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", genre='" + genre + '\'' +
-                ", language='" + language + '\'' +
-                '}';
-    }
+    String language;
 }
+//    public MovieApp() {
+//    }
+//
+//    public MovieApp(String id, String title, String genre, String language) {
+//        this.id = id;
+//        this.title = title;
+//        this.genre = genre;
+//        this.language = language;
+//    }
+//
+//    public String getId() {
+//        return id;
+//    }
+//
+//
+//    public void setId(String id) {
+//        this.id = id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getGenre() {
+//        return genre;
+//    }
+//
+//
+//    public void setGenre(String genre) {
+//        this.genre = genre;
+//    }
+//
+//    public String getLanguage() {
+//        return language;
+//    }
+//
+//
+//    public void setLanguage(String language) {
+//        this.language = language;
+//    }
+//
+//
+//    @Override
+//    public String toString() {
+//        return "MovieApp{" +
+//                "id=" + id +
+//                ", title='" + title + '\'' +
+//                ", genre='" + genre + '\'' +
+//                ", language='" + language + '\'' +
+//                '}';
+//    }
+
 
