c0662a4549612086f7eb01dd8e6619bf6ab4c19e
Your Name <youremail@domain.com>
2019-10-10 13:16:34 +0530

    twelth task

--- src/main/java/com/stackroute/config/WebConfig.java
+++ src/main/java/com/stackroute/config/WebConfig.java
@@ -1,8 +1,5 @@
 package com.stackroute.config;
 
-import org.h2.server.web.WebServlet;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 public class WebConfig {
@@ -13,12 +10,12 @@ public class WebConfig {
     @Configuration
     public class WebConfiguration {
 
-        @Bean
-        ServletRegistrationBean h2ServletRegistration(){
-            ServletRegistrationBean registrationBean=new ServletRegistrationBean(new WebServlet());
-            registrationBean.addUrlMappings("/console/*");
-            return registrationBean;
-        }
+//        @Bean
+//        ServletRegistrationBean h2ServletRegistration(){
+//            ServletRegistrationBean registrationBean=new ServletRegistrationBean(new WebServlet());
+//            registrationBean.addUrlMappings("/console/*");
+//            return registrationBean;
+//        }
 
 
     }
